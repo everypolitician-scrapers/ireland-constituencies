6bf540c85bb510c707ac466d547ec35c0afde0ea
Tony Bowden <tony@tmtm.com>
2016-12-15 07:04:17 +0000

    extract Wikidata::Area class
    Rather than subclassing Wikisnakker::Item, create our own Wikidata::Area
    class instead to contain, and delegate to, a Wikisnakker::Item

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,8 @@ require 'pry'
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
-module Wikisnakker
-  class Area < Item
+module Wikidata
+  class Area
     SKIP = %i(P17 P18 P242 P910).to_set
     # TODO, reinstate this once Wikisnakker handles co-ordinates
     SKIP << :P625
@@ -18,9 +18,13 @@ module Wikisnakker
       P576: :end_date,
     }.freeze
 
+    def initialize(item)
+      @item = item
+    end
+
     def data
       unknown_properties.each do |p|
-        warn "Unknown property for #{id}: #{p} = #{send(p).value}"
+        warn "Unknown property for #{item.id}: #{p} = #{item.send(p).value}"
       end
 
       base_data.merge(wanted_data).merge(names)
@@ -28,38 +32,38 @@ module Wikisnakker
 
     private
 
+    attr_reader :item
+
     def base_data
-      { id: id }
+      { id: item.id }
     end
 
     def names
-      labels.map { |k, v| ["name__#{k.to_s.gsub('-','_')}", v[:value]] }.to_h
+      item.labels.map { |k, v| ["name__#{k.to_s.gsub('-','_')}", v[:value]] }.to_h
     end
 
     def unknown_properties
-      properties.reject { |p| SKIP.include?(p) || WANT.key?(p) }
+      item.properties.reject { |p| SKIP.include?(p) || WANT.key?(p) }
     end
 
     def wanted_properties
-      properties.select { |p| WANT.key?(p) }
+      item.properties.select { |p| WANT.key?(p) }
     end
 
     def wanted_data
-      wanted_properties.map { |p| [WANT[p], send(p).value.to_s] }.to_h
+      wanted_properties.map { |p| [WANT[p], item.send(p).value.to_s] }.to_h
     end
   end
-end
-
-module Wikidata
-  require 'wikisnakker'
 
   class Areas
+    require 'wikisnakker'
+
     def initialize(ids:)
       @ids = ids
     end
 
-    def areas
-      wikisnakker_items.flat_map(&:data).compact
+    def data
+      wikidata_areas.flat_map(&:data).compact
     end
 
     private
@@ -67,7 +71,11 @@ module Wikidata
     attr_reader :ids
 
     def wikisnakker_items
-      @wsitems ||= Wikisnakker::Area.find(ids)
+      @wsitems ||= Wikisnakker::Item.find(ids)
+    end
+
+    def wikidata_areas
+      @wdareas ||= wikisnakker_items.map { |i| Wikidata::Area.new(i) }
     end
   end
 end
